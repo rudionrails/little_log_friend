fd4b5bbdb68ee2b9b8392d59430e0e093d9ca057
Rudolf Schmidt <me@rudionrails.com>
2009-02-27 22:41:32 +0100

    made the class work properly

--- lib/little_log_friend.rb
+++ lib/little_log_friend.rb
@@ -1,71 +1,98 @@
 require 'logger'
 
-class LittleLogFriend
+module LittleLogFriend
   
   VERSION = "0.0.1"
-
-  @@colorize = false
-
-  Format = "%s [%5s] %d %s: %s"
   
-  COLORS = {
-    'DEBUG'   => "\e[1;32;1m", # green
-    'INFO'    => "\e[0;1m",    # white
-    'WARN'    => "\e[1;33;1m", # yello
-    'ERROR'   => "\e[1;31;1m", # red
-    'FATAL'   => "\e[1;35;1m", # punk, yes PUNK!
-    'UNKNOWN' => "\e[0;1m",    # white
-    'DEFAULT' => "\e[0m"       # NONE
-  } unless defined? COLORS
+  # not yet implemented
+  # def self.colorize!; Formatter.colorize!; end  
   
-  def self.message_for(severity, time, progname, msg)
-    msg = Format % [time.strftime("%Y-%m-%d %H:%M:%S"), severity, $$, progname, msg]
+  class Formatter < Logger::Formatter
     
-    msg = COLORS[severity] + msg + COLORS['DEFAULT'] if @@colorize
-    msg + "\n"
-  end
-
-  def self.colorize!; @@colorize = true; end
-  
-end
-
-# Overload the default logger class 
-class Logger  
-  class Formatter
+    @@colorize = false
     
-    # we overload the method to output our own log format
-    def call(severity, time, progname, msg)
-      LittleLogFriend.message_for(severity, time, progname, msg2str(msg))
+    Format = "%s [%5s] %d %s: %s"
+    
+    COLORS = {
+      'DEBUG'   => "\e[1;32;1m", # green
+      'INFO'    => "\e[0;1m",    # white
+      'WARN'    => "\e[1;33;1m", # yello
+      'ERROR'   => "\e[1;31;1m", # red
+      'FATAL'   => "\e[1;35;1m", # punk, yes PUNK!
+      'UNKNOWN' => "\e[0;1m",    # white
+      'DEFAULT' => "\e[0m"       # NONE
+    } unless defined? COLORS
+    
+    def initialize
+      super
+      @datetime_format = "%Y-%m-%d %H:%M:%S"
+    end
+
+    def self.colorize!; @@colorize = true; end
+    
+    # This method is invoked when a log event occurs
+    def call ( severity, time, progname, msg )
+      msg = Format % [format_datetime(time), severity, $$, progname, msg2str(msg)]
+      
+      # msg = COLORS[severity] + msg + COLORS['DEFAULT'] if @@colorize
+      msg << "\n"
     end
     
-  end
-end
-
-# Overload the BufferedLogger
-module ActiveSupport
-  class BufferedLogger
-    
-    # adding level_to_s to the already existing Severity module
-    module Severity
-      def level_to_s(level)
-        case level
+    def number_to_severity ( n )
+      case n
         when 0 : 'DEBUG'
         when 1 : 'INFO'
         when 2 : 'WARN'
         when 3 : 'ERROR'
         when 4 : 'FATAL'
         when 5 : 'UNKNOWN'
-        end
       end
     end
+  end
+  
+end
+
+# Overload the default logger class 
+class Logger  
+  
+  alias :old_formatter :formatter if method_defined?(:formatter)
+  
+  def formatter
+    @formatter ||= LittleLogFriend::Formatter.new
+  end
+  
+  
+  private
+
+  alias :old_format_message :format_message if method_defined?(:format_message)
+
+  def format_message(severity, datetime, progname, msg)
+    (formatter || @default_formatter).call(severity, datetime, progname, msg)
+  end
+  
+end
+
+# Overload the BufferedLogger in Rails
+module ActiveSupport
+  class BufferedLogger    
+    
+    alias :old_formatter :formatter if method_defined?(:formatter)
+    
+    def formatter
+      @formatter ||= LittleLogFriend::Formatter.new
+    end
+    
+    
+    alias :old_add :add if method_defined?(:add)
     
     # We need to overload the add method. Basibally it is the same as the 
     # original one, but we add our own log format to it.
     def add(severity, message = nil, progname = nil, &block)
       return if @level > severity
       message = (message || (block && block.call) || progname).to_s
-      message = LittleLogFriend.message_for(level_to_s(severity), Time.now.utc, progname, message)
-      @buffer << message
+      message = formatter.call(formatter.number_to_severity(severity), Time.now.utc, progname, message)
+      message = "#{message}\n" unless message[-1] == ?\n
+      buffer << message
       auto_flush
       message
     end
